2c47cab9ed08347291f3ea54c67f9f9e79da42b5
sKamiJ <790028837@qq.com>
2017-08-10 01:50:49 +0800

    add bst

--- bst.c
+++ bst.c
@@ -56,9 +56,9 @@ void bst_create(BST* bst) {
  * @param root 根节点地址
  *
  */
-static void bst_clear_internal(BSTNode** root){
+static void bst_clear_internal(BSTNode** root) {
 	//根节点为NULL时，递归结束
-	if(!*root){
+	if (!*root) {
 		return;
 	}
 	//清除左子树
@@ -103,11 +103,11 @@ void bst_destroy(BST* bst) {
  * @return 节点数量
  *
  */
-static int bst_size_internal(const BSTNode* root){
-	int lsize,rsize;
+static int bst_size_internal(const BSTNode* root) {
+	int lsize, rsize;
 
 	//根节点为NULL时，返回0
-	if(!root){
+	if (!root) {
 		return 0;
 	}
 	//获取左子树节点数量
@@ -127,7 +127,7 @@ static int bst_size_internal(const BSTNode* root){
  * @return 节点数量
  *
  */
-int bst_size(const BST* bst){
+int bst_size(const BST* bst) {
 	return bst_size_internal(bst->root);
 }
 
@@ -140,11 +140,11 @@ int bst_size(const BST* bst){
  * @return 高度
  *
  */
-static int bst_height_internal(const BSTNode* root){
-	int lheight,rheight;
+static int bst_height_internal(const BSTNode* root) {
+	int lheight, rheight;
 
 	//根节点为NULL时，返回0
-	if(!root){
+	if (!root) {
 		return 0;
 	}
 	//获取左子树高度
@@ -164,7 +164,7 @@ static int bst_height_internal(const BSTNode* root){
  * @return 高度
  *
  */
-int bst_height(const BST* bst){
+int bst_height(const BST* bst) {
 	return bst_height_internal(bst->root);
 }
 
@@ -177,15 +177,15 @@ int bst_height(const BST* bst){
  * @return 叶子数量
  *
  */
-static int bst_leaf_count_internal(const BSTNode* root){
-	int lleaf,rleaf;
+static int bst_leaf_count_internal(const BSTNode* root) {
+	int lleaf, rleaf;
 
 	//根节点为NULL时，返回0
-	if(!root){
+	if (!root) {
 		return 0;
 	}
 	//根节点为叶子节点时，返回1
-	if(!root->lchild && !root->rchild){
+	if (!root->lchild && !root->rchild) {
 		return 1;
 	}
 	//获取左子树叶子数量
@@ -205,7 +205,7 @@ static int bst_leaf_count_internal(const BSTNode* root){
  * @return 叶子数量
  *
  */
-int bst_leaf_count(const BST* bst){
+int bst_leaf_count(const BST* bst) {
 	return bst_leaf_count_internal(bst->root);
 }
 
@@ -221,8 +221,8 @@ int bst_leaf_count(const BST* bst){
  *         大于0 k1>k2
  *
  */
-static int bst_key_cmp(const BSTKey* k1, const BSTKey* k2){
-	return strcmp(k1->key,k2->key);
+static int bst_key_cmp(const BSTKey* k1, const BSTKey* k2) {
+	return strcmp(k1->key, k2->key);
 }
 
 /**
@@ -237,15 +237,16 @@ static int bst_key_cmp(const BSTKey* k1, const BSTKey* k2){
  *         FALSE 内存不足或键已经存在，插入失败
  *
  */
-static BOOL bst_insert_internal(BSTNode** root,const BSTKey* key, const BSTVal* val){
+static BOOL bst_insert_internal(BSTNode** root, const BSTKey* key,
+		const BSTVal* val) {
 	int res;
 
 	//根节点为NULL时，插入键值对
-	if(!*root){
+	if (!*root) {
 		//新建节点
-		*root = (BSTNode*)malloc(sizeof(BSTNode));
+		*root = (BSTNode*) malloc(sizeof(BSTNode));
 		//内存不足时，错误
-		if(!*root){
+		if (!*root) {
 			return FALSE;
 		}
 		//左右孩子节点为NULL
@@ -256,13 +257,13 @@ static BOOL bst_insert_internal(BSTNode** root,const BSTKey* key, const BSTVal* 
 		return TRUE;
 	}
 	//比较键和该节点键的大小
-	res = bst_key_cmp(key,&(*root)->k);
-	if(res<0){
+	res = bst_key_cmp(key, &(*root)->k);
+	if (res < 0) {
 		//小于该节点键时，在左子树插入
-		return bst_insert_internal(&(*root)->lchild,key,val);
-	} else if(res > 0){
+		return bst_insert_internal(&(*root)->lchild, key, val);
+	} else if (res > 0) {
 		//大于该节点键时，在右子树插入
-		return bst_insert_internal(&(*root)->rchild,key,val);
+		return bst_insert_internal(&(*root)->rchild, key, val);
 	} else {
 		//等于该节点键，失败
 		return FALSE;
@@ -281,8 +282,8 @@ static BOOL bst_insert_internal(BSTNode** root,const BSTKey* key, const BSTVal* 
  *         FALSE 内存不足或键已经存在，插入失败
  *
  */
-BOOL bst_insert(BST* bst, const BSTKey* key, const BSTVal* val){
-	return bst_insert_internal(&bst->root,key,val);
+BOOL bst_insert(BST* bst, const BSTKey* key, const BSTVal* val) {
+	return bst_insert_internal(&bst->root, key, val);
 }
 
 /**
@@ -296,29 +297,29 @@ BOOL bst_insert(BST* bst, const BSTKey* key, const BSTVal* val){
  *         FALSE 键不存在，删除失败
  *
  */
-static BOOL bst_remove_internal(BSTNode** root,const BSTKey* key){
+static BOOL bst_remove_internal(BSTNode** root, const BSTKey* key) {
 	BSTNode *p, *q, *del;
 	int res;
 
 	//根节点为NULL时，无该键，错误
-	if(!*root){
+	if (!*root) {
 		return FALSE;
 	}
 	//比较键和该节点键的大小
-	res = bst_key_cmp(key,&(*root)->k);
-	if(res < 0){
+	res = bst_key_cmp(key, &(*root)->k);
+	if (res < 0) {
 		//小于该节点键时，在左子树删除
-		return bst_remove_internal(&(*root)->lchild,key);
-	}else if(res >0){
+		return bst_remove_internal(&(*root)->lchild, key);
+	} else if (res > 0) {
 		//大于该节点键时，在右子树删除
-		return bst_remove_internal(&(*root)->rchild,key);
+		return bst_remove_internal(&(*root)->rchild, key);
 	} else {
 		//等于该节点键，删除该节点
 		del = *root;
-		if(!del->lchild){
+		if (!del->lchild) {
 			//删除节点无左孩子时，根节点为其右孩子
 			*root = del->rchild;
-		} else if(!del->rchild){
+		} else if (!del->rchild) {
 			//删除节点无右孩子时，根节点为其左孩子
 			*root = del->lchild;
 		} else {
@@ -326,14 +327,14 @@ static BOOL bst_remove_internal(BSTNode** root,const BSTKey* key){
 			//p为最右节点的父节点
 			p = del;
 			//q为最右节点
-			q=p->lchild;
+			q = p->lchild;
 			//寻找最右节点
-			while(q->rchild){
+			while (q->rchild) {
 				p = q;
 				q = q->rchild;
 			}
 			//删除节点的左孩子不为最右节点时
-			if(p!=del){
+			if (p != del) {
 				//父节点的右孩子为最右节点的左孩子
 				p->rchild = q->lchild;
 				//最右节点的左孩子为删除节点的左孩子
@@ -361,8 +362,8 @@ static BOOL bst_remove_internal(BSTNode** root,const BSTKey* key){
  *         FALSE 键不存在，删除失败
  *
  */
-BOOL bst_remove(BST* bst,const BSTKey* key){
-	return bst_remove_internal(&bst->root,key);
+BOOL bst_remove(BST* bst, const BSTKey* key) {
+	return bst_remove_internal(&bst->root, key);
 }
 
 /**
@@ -377,22 +378,22 @@ BOOL bst_remove(BST* bst,const BSTKey* key){
  *         FALSE 键不存在，查找失败
  *
  */
-static BOOL bst_search_internal(BSTNode* root, const BSTKey* key, BSTVal** val){
+static BOOL bst_search_internal(BSTNode* root, const BSTKey* key, BSTVal** val) {
 	int res;
 
 	//根节点为NULL时，无该键，错误
-	if(!root){
+	if (!root) {
 		return FALSE;
 	}
 	//比较键和该节点键的大小
-	res = bst_key_cmp(key,&root->k);
-	if(res<0){
+	res = bst_key_cmp(key, &root->k);
+	if (res < 0) {
 		//小于该节点键时，在左子树查找
-		return bst_search_internal(root->lchild,key,val);
-	}else if(res>0){
+		return bst_search_internal(root->lchild, key, val);
+	} else if (res > 0) {
 		//大于该节点键时，在右子树查找
-		return bst_search_internal(root->rchild,key,val);
-	} else{
+		return bst_search_internal(root->rchild, key, val);
+	} else {
 		//等于该节点键，保存值的指针
 		*val = &root->v;
 		return TRUE;
@@ -411,42 +412,244 @@ static BOOL bst_search_internal(BSTNode* root, const BSTKey* key, BSTVal** val){
  *         FALSE 键不存在，查找失败
  *
  */
-BOOL bst_search(const BST* bst, const BSTKey* key, BSTVal** val){
-	return bst_search_internal(bst->root,key,val);
+BOOL bst_search(const BST* bst, const BSTKey* key, BSTVal** val) {
+	return bst_search_internal(bst->root, key, val);
 }
 
-static void bst_preorder(){
+/**
+ *
+ * 前序遍历二叉查找树内部递归函数
+ *
+ * @param root 根节点地址
+ *        visit 遍历函数指针
+ *
+ */
+static void bst_preorder_internal(BSTNode* root,
+		void (*visit)(const BSTKey* key, BSTVal* val)) {
+	//根节点为NULL时，返回
+	if (!root) {
+		return;
+	}
+	//访问根节点键值对
+	(*visit)(&root->k, &root->v);
+	//前序遍历左子树
+	bst_preorder_internal(root->lchild, visit);
+	//前序遍历右子树
+	bst_preorder_internal(root->rchild, visit);
+}
+
+/**
+ *
+ * 前序遍历二叉查找树
+ *
+ * @param bst 二叉查找树地址
+ *        visit 遍历函数指针
+ *
+ */
+void bst_preorder(const BST* bst, void (*visit)(const BSTKey* key, BSTVal* val)) {
+	bst_preorder_internal(bst->root, visit);
+}
+
+/**
+ *
+ * 中序遍历二叉查找树内部递归函数
+ *
+ * @param root 根节点地址
+ *        visit 遍历函数指针
+ *
+ */
+static void bst_inorder_internal(BSTNode* root,
+		void (*visit)(const BSTKey* key, BSTVal* val)) {
+	//根节点为NULL时，返回
+	if (!root) {
+		return;
+	}
+	//中序遍历左子树
+	bst_inorder_internal(root->lchild, visit);
+	//访问根节点键值对
+	(*visit)(&root->k, &root->v);
+	//中序遍历右子树
+	bst_inorder_internal(root->rchild, visit);
+}
+
+/**
+ *
+ * 中序遍历二叉查找树
+ *
+ * @param bst 二叉查找树地址
+ *        visit 遍历函数指针
+ *
+ */
+void bst_inorder(const BST* bst, void (*visit)(const BSTKey* key, BSTVal* val)) {
+	bst_inorder_internal(bst->root, visit);
+}
+
+/**
+ *
+ * 后序遍历二叉查找树内部递归函数
+ *
+ * @param root 根节点地址
+ *        visit 遍历函数指针
+ *
+ */
+static void bst_postorder_internal(BSTNode* root,
+		void (*visit)(const BSTKey* key, BSTVal* val)) {
+	//根节点为NULL时，返回
+	if (!root) {
+		return;
+	}
+	//后序遍历左子树
+	bst_postorder_internal(root->lchild, visit);
+	//后序遍历右子树
+	bst_postorder_internal(root->rchild, visit);
+	//访问根节点键值对
+	(*visit)(&root->k, &root->v);
+}
+
+/**
+ *
+ * 后序遍历二叉查找树
+ *
+ * @param bst 二叉查找树地址
+ *        visit 遍历函数指针
+ *
+ */
+void bst_postorder(const BST* bst,
+		void (*visit)(const BSTKey* key, BSTVal* val)) {
+	bst_postorder_internal(bst->root, visit);
+}
+
+/**
+ *
+ * 层序遍历二叉查找树
+ *
+ * @param bst 二叉查找树地址
+ *        visit 遍历函数指针
+ *
+ */
+void bst_level(const BST* bst, void (*visit)(const BSTKey* key, BSTVal* val)) {
+	int size;
+	BSTNode* p;
+	BSTNode** q;
+	int front, rear;
+
+	//根节点为NULL时，返回
+	if (!bst->root) {
+		return;
+	}
+	//获取节点数
+	size = bst_size(bst);
+	//创建一个队列，这里根据节点数申请内存
+	q = (BSTNode**) malloc(sizeof(BSTNode*) * size);
+	//0位置元素为根节点
+	q[0] = bst->root;
+	//队首位置为0
+	front = 0;
+	//队尾位置为1
+	rear = 1;
+	//当队列不为空时
+	while (front < rear) {
+		//获取队首节点
+		p = q[front++];
+		//左孩子不为空时，添加至队尾
+		if (p->lchild) {
+			q[rear++] = p->lchild;
+		}
+		//右孩子不为空时，添加至队尾
+		if (p->rchild) {
+			q[rear++] = p->rchild;
+		}
+		//访问队首节点键值对
+		(*visit)(&p->k, &p->v);
+	}
+	//释放队列
+	free(q);
+}
+
+/**
+ *
+ * 遍历函数，可以修改，或建立类似函数
+ *
+ * @param key 键
+ *        val 值
+ *
+ */
+static void bst_visit(const BSTKey* key, BSTVal* val) {
+	printf("%s %d\n", key->key, val->val);
+}
+
+/**
+ *
+ * 显示二叉查找树信息，可以修改
+ *
+ * @param bst 二叉查找树地址
+ *
+ */
+void bst_show(const BST* bst) {
+	printf("节点数：%d\n", bst_size(bst));
+	printf("高度：%d\n", bst_height(bst));
+	printf("叶子数：%d\n", bst_leaf_count(bst));
+	printf("前序遍历结果：\n");
+	bst_preorder(bst, bst_visit);
+	printf("中序遍历结果：\n");
+	bst_inorder(bst, bst_visit);
+	printf("后序遍历结果：\n");
+	bst_postorder(bst, bst_visit);
+	printf("层序遍历结果：\n");
+	bst_level(bst, bst_visit);
+	printf("\n");
 }
 
 int main() {
-	//Queue q;
-	//QueueElem e;
-	//QueueElem* ep;
-	//int i;
+	BST b;
+	BSTKey k;
+	BSTVal v;
+	BSTVal* vp;
 
-	////创建队列
-	//queue_create(&q);
+	//创建二叉查找树
+	bst_create(&b);
 
-	////元素入队列
-	//for(i=0;i<100;++i){
-	//	e.val = i;
-	//	queue_enqueue(&q,&e);
-	//}
-	//queue_show(&q);
+	//插入键值对
+	printf("插入键值对：\n");
+	strcpy(k.key, "boy");
+	v.val = 1;
+	bst_insert(&b, &k, &v);
+	bst_show(&b);
 
-	//while(!queue_empty(&q)){
-	//	queue_dequeue(&q);
-	//}
-	//queue_show(&q);
+	strcpy(k.key, "apple");
+	v.val = 2;
+	bst_insert(&b, &k, &v);
+	bst_show(&b);
 
-	//for(i=0;i<100;++i){
-	//	e.val = i;
-	//	queue_enqueue(&q,&e);
-	//}
-	//queue_show(&q);
+	strcpy(k.key, "dog");
+	v.val = 3;
+	bst_insert(&b, &k, &v);
+	bst_show(&b);
 
-	////销毁链队列
-	//queue_destroy(&q);
+	strcpy(k.key, "cat");
+	v.val = 4;
+	bst_insert(&b, &k, &v);
+	bst_show(&b);
+
+	//获取键，更改其值
+	printf("获取键，更改其值：\n");
+	strcpy(k.key, "dog");
+	bst_search(&b, &k, &vp);
+	vp->val = 5;
+	bst_show(&b);
+
+	//删除键
+	printf("删除键：\n");
+	bst_remove(&b, &k);
+	bst_show(&b);
+
+	//清除所有元素
+	printf("清除所有元素：\n");
+	bst_clear(&b);
+	bst_show(&b);
+
+	//销毁二叉查找树
+	bst_destroy(&b);
 
 	return 0;
 }
